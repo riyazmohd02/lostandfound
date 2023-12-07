e612ff6bbba6a7a29c8f697329b513c8b58c4705
Anoohhya Choudavaram <anoohhya.c@fpt.com>
2023-12-07 09:50:05 +0530

    changes in profile page, edit button

--- lost_found_frontend/My_profile/my_profile.js
+++ lost_found_frontend/My_profile/my_profile.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const profilePicture = document.getElementById("profilePicture");
   const editImageInput = document.getElementById("editImage");
   const inputFields = document.querySelectorAll('.form-control');
-  const saveChangesBtn = document.getElementById('saveChangesBtn');
+  const saveButton = document.getElementById("save-button");
 
   inputFields.forEach(function (field) {
     field.addEventListener('input', handleInputChange);
@@ -14,43 +14,42 @@ document.addEventListener("DOMContentLoaded", function () {
       return field.value !== field.defaultValue;
     });
 
+    // Check if the profile picture has changed
+    const profilePictureChanged = profilePicture.src !== "../Images/2.png";
+
     // Enable or disable the "Save Changes" button based on changes
-    saveChangesBtn.disabled = !changesMade;
+    saveButton.disabled = !(changesMade || profilePictureChanged);
+    saveButton.classList.toggle('active', (changesMade || profilePictureChanged));
   }
-});
-// Add an event listener to the edit icon label
-profilePicture.addEventListener("click", function () {
-  editImageInput.click();
-});
 
-// Handle the selected image file
-editImageInput.addEventListener("change", function (event) {
-  const selectedImage = event.target.files[0];
+  // Handle the selected image file and update the profile picture
+  editImageInput.addEventListener("change", function (event) {
+    const selectedImage = event.target.files[0];
 
-  if (selectedImage) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      profilePicture.src = e.target.result;
-    };
-    reader.readAsDataURL(selectedImage);
-  }
-});
-
-// Add functionality for the pencil icon
-const pencilIcons = document.querySelectorAll('.pencil-icon');
-pencilIcons.forEach(icon => {
-  icon.addEventListener('click', function () {
-    const inputField = icon.parentElement.querySelector('.with-icon');
-    inputField.focus();
+    if (selectedImage) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        profilePicture.src = e.target.result;
+        handleInputChange(); // Trigger input change handling
+      };
+      reader.readAsDataURL(selectedImage);
+    }
   });
+
+  // Add functionality for the pencil icon
+  const pencilIcons = document.querySelectorAll('.pencil-icon');
+  pencilIcons.forEach(icon => {
+    icon.addEventListener('click', function () {
+      const inputField = icon.parentElement.querySelector('.with-icon');
+      inputField.focus();
+    });
+  });
+
+  function goBack() {
+    window.history.back();
+  }
+
 });
-
-
-
-
-function goBack() {
-  window.history.back();
-}
 
 function enableEditMode() {
   // Enable specific input fields for editing
@@ -64,66 +63,35 @@ function enableEditMode() {
   document.getElementById("Email").setAttribute('disabled', 'disabled');
 
   // Enable the "Save Changes" button
-  document.querySelector('.registerbtn').removeAttribute('disabled');
+  document.getElementById("save-button").removeAttribute('disabled');
 }
 
 function saveChanges() {
-  // Your existing saveChanges function logic...
-  // After saving changes, disable input fields and the "Save Changes" button
-  var inputFields = document.querySelectorAll('.form-control');
+  // Disable all input fields
+  const inputFields = document.querySelectorAll('.form-control');
   inputFields.forEach(function (field) {
     field.setAttribute('disabled', 'disabled');
   });
 
-  document.querySelector('.registerbtn').setAttribute('disabled', 'disabled');
+  // Disable the "Save Changes" button
+  const saveButton = document.getElementById("save-button");
+  saveButton.setAttribute('disabled', 'disabled');
 
-  // You can also display a confirmation message if needed
-  document.getElementById('confirmationMessage').innerText = 'Changes saved successfully!';
+  // Change button color to grey and cursor to none
+  saveButton.style.backgroundColor = '#a6a6a6';
+  saveButton.style.cursor = 'not-allowed';
 
-  return false; // Prevent the form from submitting (remove this line if form submission is needed)
+  // Display a side popup message for 3 seconds
+  const popup = document.createElement("div");
+  popup.className = "popup";
+  popup.textContent = "Changes saved!";
+  document.body.appendChild(popup);
+
+  setTimeout(function () {
+    popup.remove();
+  }, 3000);
+
+  // Return false to prevent the form submission (assuming you don't want to submit the form)
+  return false;
 }
-function handleEditIconClick() {
-  document.getElementById("editImage").click();
-}
-
-function validateForm() {
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
-  var email = document.getElementById("email").value;
-  var phone = document.getElementById("phone").value;
-  var countryRegion = document.getElementById("countryRegion").value;
-
-  // Validate First Name
-  if (!/^[a-zA-Z ]+$/.test(firstName)) {
-    alert("Please enter a valid First Name (only letters and spaces allowed).");
-    return false;
-  }
-
-  // Validate Last Name
-  if (!/^[a-zA-Z ]+$/.test(lastName)) {
-    alert("Please enter a valid Last Name (only letters and spaces allowed).");
-    return false;
-  }
-
-  // Validate Email
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    alert("Please enter a valid Email address.");
-    return false;
-  }
-
-  // Validate Phone Number
-  var phoneRegex = /^\d{10}$/;
-  if (!phoneRegex.test(phone)) {
-    alert("Please enter a valid 10-digit Phone Number.");
-    return false;
-  }
-
-  // Validate Country/Region
-  if (!/^[a-zA-Z ]+$/.test(countryRegion)) {
-    alert("Please enter a valid Country/Region (only letters and spaces allowed).");
-    return false;
-  }
-
-  return true;
-}
+        
