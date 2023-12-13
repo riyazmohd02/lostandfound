98e75aa1e92512575aeea1b8dc03031db1b4667d
Anoohhya Choudavaram <anoohhya.c@fpt.com>
2023-12-13 13:12:05 +0530

    changes to login page

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "lostandfound",
+  database: "lost and found",
 });
 
 connection.connect((err) => {
--- lost_found_frontend/login/login.js
+++ lost_found_frontend/login/login.js
@@ -1,72 +1,93 @@
 const passwordField = document.getElementById('password');
-const loginButton = document.getElementById('loginButton');
-const emailField = document.querySelector('[type="text"]');
+      const loginButton = document.getElementById('loginButton');
+      const emailField = document.querySelector('[type="text"]');
 
-let passwordVisible = false;
+      let passwordVisible = false;
 
-function togglePasswordVisibility() {
-  passwordVisible = !passwordVisible;
-  passwordField.type = passwordVisible ? 'text' : 'password';
-}
-
-function displayErrorPopup(message) {
-  const errorPopup = document.createElement('div');
-  errorPopup.classList.add('alert-popup');
-  errorPopup.textContent = message;
-  document.body.appendChild(errorPopup);
-
-  setTimeout(() => {
-    errorPopup.remove();
-  }, 3000); // Display the error for 3 seconds (adjust as needed)
-}
-
-// function validateEmail(email) {
-//   // You can implement additional email validation if needed
-//   // return email.endsWith('@gmail.com');
-// }
-
-function validatePassword(password) {
-  // Password should have one capital letter, one small letter, one number, and one special character
-  const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  return regex.test(password);
-}
-
-function loginuser() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  const loginUserData = {
-    email: email,
-    password: password,
-  };
-  console.log('Result: ', loginUserData);
-
-  fetch('http://localhost:7000/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(loginUserData),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+      function togglePasswordVisibility() {
+        passwordVisible = !passwordVisible;
+        passwordField.type = passwordVisible ? 'text' : 'password';
       }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      if (data.error) {
-        console.error(data.error);
-      } else {
-        window.location.href = '../homepage/home_page.html';
-      }
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
-}
 
-function navigateToNextPage() {
-  window.location.href = "../signup/signup.html";
-}
+      function displayErrorPopup(message) {
+        const errorPopup = document.createElement('div');
+        errorPopup.classList.add('alert-popup');
+        errorPopup.textContent = message;
+        document.body.appendChild(errorPopup);
+
+        setTimeout(() => {
+          errorPopup.remove();
+        }, 3000); // Display the error for 3 seconds (adjust as needed)
+      }
+
+      function validateEmail(email) {
+        // You can implement additional email validation if needed
+        return email.endsWith('@gmail.com');
+      }
+
+      function validatePassword(password) {
+        // Password should have one capital letter, one small letter, one number, and one special character
+        const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return regex.test(password);
+      }
+
+      
+      function login() {
+        const userEmail = emailField.value.trim();
+        const userPassword = passwordField.value.trim();
+       
+        if (!userEmail && !userPassword) {
+          displayErrorPopup('Enter email and password');
+          return;
+        }
+        if (!userEmail) {
+          displayErrorPopup('Enter email ');
+          return;
+        }
+
+        if (!userPassword) {
+          displayErrorPopup('Enter password');
+          return;
+        }
+
+        if (!validateEmail(userEmail) || !validatePassword(userPassword)) {
+          displayErrorPopup('Invalid email/password');
+          return;
+        }
+
+        const loginUserData = {
+          email: userEmail,
+          password: userPassword,
+        };
+
+        fetch('http://localhost:7000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(loginUserData),
+        })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then(data => {
+            console.log(data);
+            if (data.error) {
+              displayErrorPopup(data.error); // Display error from the server
+            } else {
+              
+              window.location.href = '../home page/home_page.html';
+            }
+          })
+          .catch(error => {
+            console.log('Error:', error);
+            displayErrorPopup("User does'nt exists!")
+          });
+      }
+
+      function navigateToNextPage() {
+        window.location.href = "../signup/SignUp.html";
+      }
